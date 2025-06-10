dc3e11ddd373bb34d3b330f512771899b14ebbe5
noob7of7 <zhan71007@gmail.com>
2025-06-10 11:51:08 +0500

    dz2

--- script.js
+++ script.js
@@ -2,25 +2,25 @@ let a = 10;
 let b = 5;
 let c = 2;
 
-console.log(a+c-b);
-console.log(a*b/c);
-console.log(a%b);
+console.log(a+c-b); //7
+console.log(a*b/c); //25
+console.log(a%b); //0
 
 
 let x =7;
-console.log(x++ + ++x);
-console.log(x-- - --x);
+console.log(x++ + ++x); //16
+console.log(x-- - --x); //2
 
 
 let num1 = 4;
 let num2 = 5;
-let result = num1+num2;
-console.log("The result is: "+result);
+let result = num1+num2; //4+5
+console.log("The result is: "+result); //The result is: 9
 
 let more = (a>b)>0;
-console.log(more);
+console.log(more); //True
 let or = c<10 || 10 == 2;
-console.log(or);
+console.log(or); //true
 
 
 let num11 = 10;
@@ -31,9 +31,9 @@ console.log(num11 === num22); // тип данных стр и нам они р�
 
 let d = 4;
 let srovnenie = (d%2 == 0 || d%3 == 0)&& d%6 != 0; //true
-console.log(srovnenie);
+console.log(srovnenie); //true
 
 
 let e = 7;
 let srovnenie1 = (10<e || e<5);
-console.log (srovnenie1)
+console.log (srovnenie1) //false
